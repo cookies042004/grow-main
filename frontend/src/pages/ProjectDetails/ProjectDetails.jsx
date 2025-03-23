4cb4710912d54e157e38b2c49f67d775dad8a135
Akhil Puri <ap4723@srmist.edu.in>
2025-03-23 16:16:23 +0530

    updated project

--- frontend/src/pages/ProjectDetails/ProjectDetails.jsx
+++ frontend/src/pages/ProjectDetails/ProjectDetails.jsx
@@ -193,7 +193,7 @@ export const ProjectDetails = () => {
                     <img
                       src={property?.dp}
                       alt="dealer"
-                      className="h-16 w-16 sm:h-20 sm:w-20 object-cover"
+                      className="h-16 w-16 sm:h-20 sm:w-20 object-contain"
                     />
                     <div className="flex flex-col">
                       <h1 className="flex items-center justify-center sm:justify-start font-bold text-[#1d2a3b] text-lg sm:text-xl py-[1px] sm:pl-2">
