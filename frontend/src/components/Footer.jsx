b9893cbc981e11fbb0a4575aa0e743f442f5a731
Akhil Puri <ap4723@srmist.edu.in>
2025-03-27 19:01:35 +0530

    updated css

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -13,7 +13,7 @@ import "./Footer.css";
 export const Footer = () => {
   return (
     <footer className="bg-[#1d2a3b] text-white">
-      <div className="px-5 pt-10 mx-auto max-w-screen-xl md:px-10 lg:px-9">
+      <div className="px-5 pt-5 mx-auto max-w-screen-xl md:px-10 lg:px-9">
         <div className="grid gap-10 mb-8 lg:grid-cols-6">
           {/* Left Section - Logo & About */}
           <div className="lg:col-span-2 flex flex-col items-center lg:items-start text-center lg:text-left">
@@ -83,57 +83,56 @@ export const Footer = () => {
                     growinfinityrealtor1@gmail.com
                   </a>
                 </li>
-                <li className="flex items-center text-gray-300">
+                <li className="flex items-center text-gray-300 whitespace-nowrap gap-2">
                   <PhoneIcon />
-                  <a href="tel:+919990052554" className="ml-2">
+                  <a href="tel:+919990052554" className="text-sm">
                     +91-9990052554
                   </a>
                 </li>
               </ul>
-              
 
-            {/* Follow Us Section (Properly Aligned) */}
-            <div className="flex flex-col mt-5">
-              <p className="font-semibold text-lg mb-2">Follow Us</p>
-              <div className="flex space-x-5">
-                <a href="#" target="_blank" rel="noopener noreferrer">
-                  <img
-                    src={twitter}
-                    alt="Twitter"
-                    className="w-[25px] h-[25px]"
-                  />
-                </a>
-                <a
-                  href="https://www.instagram.com/growinfinityrealtors_official/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    className="w-[25px] h-[25px]"
-                  />
-                </a>
-                <a
-                  href="https://www.facebook.com/p/Grow-Infinity-Realtors-100092248133482/?_rdr"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    className="w-[25px] h-[25px]"
-                  />
-                </a>
-                <a href="#" target="_blank" rel="noopener noreferrer">
-                  <img
-                    src={youtube}
-                    alt="YouTube"
-                    className="w-[25px] h-[25px]"
-                  />
-                </a>
+              {/* Follow Us Section (Properly Aligned) */}
+              <div className="flex flex-col mt-5">
+                <p className="font-semibold text-lg mb-2">Follow Us</p>
+                <div className="flex space-x-5">
+                  <a href="#" target="_blank" rel="noopener noreferrer">
+                    <img
+                      src={twitter}
+                      alt="Twitter"
+                      className="w-[25px] h-[25px]"
+                    />
+                  </a>
+                  <a
+                    href="https://www.instagram.com/growinfinityrealtors_official/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      src={instagram}
+                      alt="Instagram"
+                      className="w-[25px] h-[25px]"
+                    />
+                  </a>
+                  <a
+                    href="https://www.facebook.com/p/Grow-Infinity-Realtors-100092248133482/?_rdr"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      src={facebook}
+                      alt="Facebook"
+                      className="w-[25px] h-[25px]"
+                    />
+                  </a>
+                  <a href="#" target="_blank" rel="noopener noreferrer">
+                    <img
+                      src={youtube}
+                      alt="YouTube"
+                      className="w-[25px] h-[25px]"
+                    />
+                  </a>
+                </div>
               </div>
-            </div>
             </div>
           </div>
         </div>
